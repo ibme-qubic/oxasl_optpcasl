136cbf83631b27983edf182f39ce6508114a803d
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-09-23 15:15:57 +0100

    Add 2D mode and limit scan type options

--- oxasl_optpcasl/gui/options.py
+++ oxasl_optpcasl/gui/options.py
@@ -12,34 +12,67 @@ import wx.grid
 
 from ..structures import VAR_MULTI_PCASL, VAR_TE_PCASL, LOOK_LOCKER, VAR_TE_PCASL_NPLD, ASLParams, Scan, BATDist, Limits
 from ..optimize import LOptimal, DOptimal
-from .widgets import TabPage
+from .widgets import TabPage, NumberChooser
 
 class ScanOptions(TabPage):
     """
     Tab page for specifying the ASL scan to optimize
     """
 
+    SCAN_TYPES = {
+        "Multi-PLD PCASL" : VAR_MULTI_PCASL,
+        #"Multi-TE PCASL" : VAR_TE_PCASL,
+        "Look-Locker" : LOOK_LOCKER,
+        #"VAR_TE_PCASL_NPLD" : VAR_TE_PCASL_NPLD,
+    }
+
     def __init__(self, parent, idx, n):
         TabPage.__init__(self, parent, "Input Data", idx, n, name="input")
 
         self.section("ASL scan to optimize for")
-        self._asltype = self.choice("ASL scan type", choices=[VAR_MULTI_PCASL, VAR_TE_PCASL, LOOK_LOCKER, VAR_TE_PCASL_NPLD])
+        self._asltype = self.choice("ASL scan type", choices=list(self.SCAN_TYPES.keys()))
         self._fvalue = self.number("Estimated perfusion (ml/100g/min)", minval=0, maxval=100.0, initial=50.0)
         self._duration = self.number("Approximate scan duration (s)", minval=0, maxval=1000, initial=300)
-        self._readout = self.number("Readout time (s)", minval=0, maxval=2.0, initial=0.5)
+        self._readout_time = self.number("Readout time (s)", minval=0, maxval=2.0, initial=0.5)
         self._nplds = self.integer("Number of PLDs", minval=1, maxval=20, initial=6)
+
+        self.readout_ch = self.choice("Readout", choices=["3D (eg GRASE)", "2D multi-slice (eg EPI)"])
+        self.readout_ch.SetSelection(0)
+        self.readout_ch.Bind(wx.EVT_CHOICE, self._readout_changed)
+
+        self._nslices = self.integer("Number of slices", minval=1, maxval=100, initial=10)
+        self._slicedt = self.number("Time per slice (ms)", minval=0, maxval=50, step=1, initial=10)
+        self._readout_changed()
 
         self.sizer.AddGrowableCol(1, 1)
         self.SetSizer(self.sizer)
 
+    def _readout_changed(self, _event=None):
+        self._nslices.Enable(self.readout_ch.GetSelection() == 1)
+        self._slicedt.Enable(self.readout_ch.GetSelection() == 1)
+
+    def slicedt(self):
+        if self.readout_ch.GetSelection() == 1:
+            return self._slicedt.GetValue() / 1000
+        else:
+            return 0.0
+
+    def nslices(self):
+        if self.readout_ch.GetSelection() == 1:
+            return self._nslices.GetValue()
+        else:
+            return 1
+
     def aslparams(self):
-        return ASLParams(self._asltype.GetString(self._asltype.GetSelection()), 
+        return ASLParams(self.SCAN_TYPES[self._asltype.GetString(self._asltype.GetSelection())], 
                          self._fvalue.GetValue()/6000.0)
 
     def scan(self):
         return Scan(duration=self._duration.GetValue(), 
                     npld=self._nplds.GetValue(), 
-                    readout=self._readout.GetValue())
+                    readout=self._readout_time.GetValue(),
+                    slices=self.nslices(),
+                    slicedt=self.slicedt())
 
 class OptimizerOptions(TabPage):
     """
