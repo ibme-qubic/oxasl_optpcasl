ce319b82a2625b6b6534d1c214874e7790d453a6
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-09-23 15:16:28 +0100

    Add icon and busy cursor while running

--- oxasl_optpcasl/gui/__init__.py
+++ oxasl_optpcasl/gui/__init__.py
@@ -25,18 +25,24 @@ class OptPCASLGui(wx.Frame):
 
     def __init__(self):
         wx.Frame.__init__(self, None, title="OXASL PCASL Optimizer", size=(1100, 600), style=wx.DEFAULT_FRAME_STYLE)
-        main_panel = wx.Panel(self)
+        self._panel = wx.Panel(self)
+
+        local_dir = os.path.abspath(os.path.dirname(__file__))
+        icon = wx.Icon()
+        icon.CopyFromBitmap(wx.Bitmap(os.path.join(local_dir, "icon.png"), wx.BITMAP_TYPE_ANY))
+        self.SetIcon(icon)
+
         main_vsizer = wx.BoxSizer(wx.VERTICAL)
 
-        hpanel = wx.Panel(main_panel)
+        hpanel = wx.Panel(self._panel)
         hpanel.SetBackgroundColour((180, 189, 220))
         hsizer = wx.BoxSizer(wx.HORIZONTAL)
         hpanel.SetSizer(hsizer)
         main_vsizer.Add(hpanel, 2, wx.EXPAND)
 
-        banner = wx.Panel(main_panel, size=(-1, 80))
+        banner = wx.Panel(self._panel, size=(-1, 80))
         banner.SetBackgroundColour((180, 189, 220))
-        banner_fname = os.path.join(os.path.abspath(os.path.dirname(__file__)), "banner.png")
+        banner_fname = os.path.join(local_dir, "banner.png")
         wx.StaticBitmap(banner, -1, wx.Bitmap(banner_fname, wx.BITMAP_TYPE_ANY))
         hsizer.Add(banner)
 
@@ -45,13 +51,13 @@ class OptPCASLGui(wx.Frame):
         spacer.SetBackgroundColour((180, 189, 220))
         hsizer.Add(spacer, 10)
 
-        banner = wx.Panel(main_panel, size=(-1, 80))
+        banner = wx.Panel(self._panel, size=(-1, 80))
         banner.SetBackgroundColour((180, 189, 220))
-        banner_fname = os.path.join(os.path.abspath(os.path.dirname(__file__)), "oxasl.png")
+        banner_fname = os.path.join(local_dir, "oxasl.png")
         wx.StaticBitmap(banner, -1, wx.Bitmap(banner_fname, wx.BITMAP_TYPE_ANY))
         hsizer.Add(banner)
 
-        hpanel = wx.Panel(main_panel)
+        hpanel = wx.Panel(self._panel)
         hsizer = wx.BoxSizer(wx.HORIZONTAL)
         notebook = wx.Notebook(hpanel, id=wx.ID_ANY, style=wx.BK_DEFAULT)
 
@@ -59,7 +65,7 @@ class OptPCASLGui(wx.Frame):
         notebook.AddPage(self._scan_options, "Scan Options")
 
         self._opt_options = OptimizerOptions(notebook, 0, 1)
-        notebook.AddPage(self._opt_options, "Optimizer Options")
+        notebook.AddPage(self._opt_options, "Optimization")
 
         hsizer.Add(notebook, 1, wx.ALL|wx.EXPAND, 5)
 
@@ -68,23 +74,25 @@ class OptPCASLGui(wx.Frame):
         hpanel.SetSizer(hsizer)
         main_vsizer.Add(hpanel, 2, wx.EXPAND)
 
-        run_panel = wx.Panel(main_panel)
+        run_panel = wx.Panel(self._panel)
         runsizer = wx.BoxSizer(wx.HORIZONTAL)
         run_label = wx.StaticText(run_panel, label="")
         runsizer.Add(run_label, 1, wx.EXPAND)
-        run_btn = wx.Button(run_panel, label="Run Optimization")
-        run_btn.Bind(wx.EVT_BUTTON, self._dorun)
-        runsizer.Add(run_btn, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 5)
+        self._run_btn = wx.Button(run_panel, label="Run Optimization")
+        self._run_btn.Bind(wx.EVT_BUTTON, self._dorun)
+        runsizer.Add(self._run_btn, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, 5)
         run_panel.SetSizer(runsizer)
         main_vsizer.Add(run_panel, 0, wx.EXPAND)
         run_panel.Layout()
 
-        main_panel.SetSizer(main_vsizer)
+        self._panel.SetSizer(main_vsizer)
         self.Layout()
 
-    def _dorun(self, _):
+    def _dorun(self, _event):
         try:
-            
+            self._run_btn.Enable(False)
+            self._panel.SetCursor(wx.Cursor(wx.CURSOR_WAIT))
+            wx.Yield()
             params = self._scan_options.aslparams()
             scan = self._scan_options.scan()
 
@@ -102,6 +110,9 @@ class OptPCASLGui(wx.Frame):
             sys.stderr.write("ERROR: %s\n" % str(exc))
             import traceback
             traceback.print_exc()
+        finally:
+            self._run_btn.Enable(True)
+            self._panel.SetCursor(wx.Cursor(wx.CURSOR_ARROW))
 
 def main():
     """
