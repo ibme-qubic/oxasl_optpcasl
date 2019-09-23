93f0e25e35c11e533812b28263a5729bbd53849e
Martin Craig <martin.craig@eng.ox.ac.uk>
2019-09-23 15:17:27 +0100

    Include icon in package

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ kwargs = {
     'install_requires' : get_requirements(module_dir),
     'packages' : find_packages(),
     'package_data' : {
-        'oxasl_optpcasl.gui': ['banner.png', "oxasl.png"]
+        'oxasl_optpcasl.gui': ['banner.png', "oxasl.png", "icon.png"]
     },
     'entry_points' : {
         'console_scripts' : [
